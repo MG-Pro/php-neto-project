3fcb20ff90e14c8c83afa78bf43a6d365dd1595f
MG <mg84@bk.ru>
2018-11-19 00:03:59 +0300

    created add question form

---
+++ server/views/templates/add-question.php
@@ -0,0 +1,34 @@
+<div class="container">
+  <div class="row">
+    <div class="content col">
+      <h3>Задайте вопрос</h3>
+      <hr>
+      <form class="mb-5">
+        <div class="col-6">
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email">
+          </div>
+          <div class="form-group">
+            <label for="name">Ваше имя</label>
+            <input type="text" class="form-control" id="name">
+          </div>
+          <div class="form-group">
+            <label for="category">Категория</label>
+            <select class="form-control" id="category">
+              <option>1</option>
+            </select>
+          </div>
+        </div>
+        <div class="col-12">
+          <div class="form-group">
+            <label for="textarea">Содержание вопроса</label>
+            <textarea class="form-control" id="textarea" rows="3"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Отправить</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
