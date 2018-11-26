ca90bda112fd18ba05d5f3416611409679440b8a
MG <mg84@bk.ru>
2018-11-26 22:32:49 +0300

    add authors model

---
+++ server/models/AuthorsModel.php
@@ -0,0 +1,22 @@
+<?php
+
+class AuthorsModel {
+  private $pdo;
+  public function __construct($pdo) {
+    $this->pdo = $pdo;
+  }
+  private function request($sql) {
+    return $this->pdo->query($sql);
+  }
+  public function add($name, $email) {
+    $sqlAdd = "INSERT INTO authors SET name='$name', email='$email'";
+    $sqlLastId = "SELECT @@IDENTITY";
+    $this->request($sqlAdd)->fetchAll(PDO::FETCH_ASSOC);
+    return $this->request($sqlLastId)->fetchAll(PDO::FETCH_ASSOC);
+  }
+  public function isExist($email) {
+    $sqlIsExists = "SELECT id FROM authors WHERE email='$email' LIMIT 1";
+    return $this->request($sqlIsExists)->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+}
