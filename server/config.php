f383df385b50a7fcd75d84dda92e056ca75b6f0d
MG <mg84@bk.ru>
2018-12-09 19:42:35 +0300

    add auto config

--- server/config.php
+++ server/config.php
@@ -1,9 +1,15 @@
 <?php
 
-// define('DB_LOGIN', "root");
-// define('DB_PASS', "");
-// define('DB_NAME', "faq");
+if(strpos($_SERVER['HTTP_HOST'], 'localhost') !== false) {
+  define('DB_LOGIN', "root");
+  define('DB_PASS', "");
+  define('DB_NAME', "faq");
+} else {
+  define('DB_LOGIN', "mgladkih");
+  define('DB_PASS', "neto1853");
+  define('DB_NAME', "mgladkih");
+}
 
-define('DB_LOGIN', "mgladkih");
-define('DB_PASS', "neto1853");
-define('DB_NAME', "mgladkih");
+
+
+
